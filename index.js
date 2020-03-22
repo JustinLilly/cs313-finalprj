487d5d7be390fc886dafc7dd80e5fecb329f2a52
JustinLilly <lil16003@byui.edu>
2020-03-21 19:29:16 -0600

    following Using Heroku Databases tutorial

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+require('dotenv').config();
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +10,23 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+  const connectionString = process.env.DATABASE_URL;
+
+  var sql = "SELECT * FROM fp_user";
+
+  pool.query(sql, function(err, result) {
+      // If an error occurred...
+      if (err) {
+          console.log("Error in query: ")
+          console.log(err);
+      }
+
+      // Log this to the console for debugging purposes.
+      console.log("Back from DB with result:");
+      console.log(result.rows);
+
+
+  }); 
